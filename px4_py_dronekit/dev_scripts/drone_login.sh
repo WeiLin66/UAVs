492ed893bf2076ba1890eee92c13a64e13f10f5e
wk94095 <54427703+wk94095@users.noreply.github.com>
2022-01-22 20:47:32 +0800

    Update drone_login.sh

--- px4_py_dronekit/dev_scripts/drone_login.sh
+++ px4_py_dronekit/dev_scripts/drone_login.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -k -X POST "https://10.1.181.230/apis/drone/checkin/" -H "accept: application/json" -H "APIKey: AWFJ1hYT4r8XRsxUOKXpcA" -H "Content-Type: application/json" -d "{\"IP\":\"10.27.0.13\",\"RTSPURL\":\"\",\"SerialNo\":\"oring-000001\"}"
+curl -k -X POST "https://10.1.181.230/apis/drone/checkin/" -H "accept: application/json" -H "APIKey: AWFJ1hYT4r8XRsxUOKXpcA" -H "Content-Type: application/json" -d "{\"IP\":\"10.27.0.13\",\"RTSPURL\":\"rtsp://10.27.0.13:554/test.mp4\",\"SerialNo\":\"oring-000001\"}"
